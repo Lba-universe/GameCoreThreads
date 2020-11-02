359f03f668617de5921777a982b47b428440a620
AviBoter <maccavi2@gmail.com>
2020-11-02 20:27:24 +0200

    firstGame_Ve2

--- Assets/Move2D.cs
+++ Assets/Move2D.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Player moving methods
 public class Move2D : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -13,13 +13,13 @@ public class Move2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y<10)
             transform.position += new Vector3(0, 1, 0);
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > -10)
             transform.position += new Vector3(0, -1, 0);
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > -18)
             transform.position += new Vector3(-1, 0, 0);
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < 15)
             transform.position += new Vector3(1, 0, 0);
     }
 }
